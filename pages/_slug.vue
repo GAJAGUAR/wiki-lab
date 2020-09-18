9a9c5616d4c4c14e0f54d1a1ae4a9b6ff617c379
G.A.JAGUAR <dev.gajaguar@gmail.com>
2020-09-18 14:57:18 -0500

    Creates basic dynamic page for content

---
+++ pages/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1 v-if="page.title !== null" class="title">{{ page.title }}</h1>
+    <h2 v-if="page.subtitle !== null" class="subtitle">{{ page.subtitle }}</h2>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const page = await $content(params.slug).fetch()
+
+    return { page }
+  },
+}
+</script>
